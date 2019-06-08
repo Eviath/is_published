308736e1798b2e555b5f3e852c58dc5ae74d7c01
Jacek Kowalski <eviathos@gmail.com>
2019-06-08 14:30:10 +0200

    Initial gem commit

---
+++ lib/is_published.rb
@@ -0,0 +1,6 @@
+require "is_published/version"
+
+module IsPublished
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/is_published/version.rb
@@ -0,0 +1,3 @@
+module IsPublished
+  VERSION = "0.1.0"
+end
