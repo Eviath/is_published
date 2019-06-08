43c36259ca3fd52f51023e8ceb47ae0e85a0f668
Jacek <eviathos@gmail.com>
2019-06-08 14:55:45 +0200

    rspec tests for gem
    Rspec tests passing

--- spec/is_published_spec.rb
+++ spec/is_published_spec.rb
@@ -3,7 +3,36 @@ RSpec.describe IsPublished do
     expect(IsPublished::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+
+  before :each do
+    ActiveRecord::Base.establish_connection(
+      adapter: 'sqlite3',
+      database: ':memory'
+    )
+    ActiveRecord::Schema.define do
+      create_table :posts, force: true do |t|
+        t.string :published, default: false
+      end
+    end
   end
+
+  describe '.published' do
+    before :each do
+      class Post < ActiveRecord::Base
+        extend IsPublished::Scopes
+      end
+    end
+
+    it 'returns records where published: true' do
+      post1 = Post.create
+      post2 = Post.create(published: true)
+
+      posts = Post.published
+
+      expect(posts.size).to eq(1)
+      expect(posts).to include(post2)
+      expect(Post.published.to_a).to_not include(post1)
+    end
+  end
+
 end
